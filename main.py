623b2a7644d28373e2c9daaa2eddc7a3340b3df3
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2024-02-20 20:04:02 +1000

    Functions created

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+def get_numbers():
+    pass
+
+
+def square_numbers(numbers):
+    pass
+
+
+def display_numbers(numbers):
+    pass
+
+
 def main():
     numbers = get_numbers()
     square_numbers(numbers)
     display_numbers(numbers)
 
+# Like garlic bread
